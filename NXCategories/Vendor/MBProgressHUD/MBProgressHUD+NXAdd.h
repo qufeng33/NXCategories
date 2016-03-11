6bcca65adbf62da9e823aea3ada5d010e4bbb3f7
nightx <qufeng33@hotmail.com>
2016-03-11 16:51:38 +0800

    add MBProgressHUDCategories

---
+++ NXCategories/Vendor/MBProgressHUD/MBProgressHUD+NXAdd.h
@@ -0,0 +1,13 @@
+//
+//  MBProgressHUD+NXAdd.h
+//  NXCategoriesDemo
+//
+//  Created by 蒋瞿风 on 16/3/11.
+//  Copyright © 2016年 nightx. All rights reserved.
+//
+
+#import <MBProgressHUD/MBProgressHUD.h>
+
+@interface MBProgressHUD (NXAdd)
+
+@end
---
+++ NXCategories/Vendor/MBProgressHUD/MBProgressHUD+NXAdd.m
@@ -0,0 +1,13 @@
+//
+//  MBProgressHUD+NXAdd.m
+//  NXCategoriesDemo
+//
+//  Created by 蒋瞿风 on 16/3/11.
+//  Copyright © 2016年 nightx. All rights reserved.
+//
+
+#import "MBProgressHUD+NXAdd.h"
+
+@implementation MBProgressHUD (NXAdd)
+
+@end
