55b67b382190199d614b433da1ce837e8abde824
nightx <qufeng33@hotmail.com>
2016-03-17 14:55:03 +0800

    add flat Color

--- NXCategories/UIKit/UIColor+NXAdd.h
+++ NXCategories/UIKit/UIColor+NXAdd.h
@@ -63,5 +63,346 @@ NS_ASSUME_NONNULL_BEGIN
  */
 + (nullable UIColor *)colorWithHexString:(NSString *)hexStr;
 
+#pragma mark - Light Shades
+
+/**
+ *  Returns a flat color object whose HSB values are 0.0, 0.0, 0.17 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatBlackColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.62, 0.50, 0.63 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatBlueColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.07, 0.45, 0.37 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatBrownColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.07, 0.31, 0.64 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatCoffeeColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.38, 0.45, 0.37 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatForestGreenColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.51, 0.10, 0.65 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatGrayColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.40, 0.77, 0.80 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatGreenColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.21, 0.70, 0.78 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatLimeColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.79, 0.51, 0.71 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatMagentaColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.01, 0.65, 0.47 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatMaroonColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.47, 0.86, 0.74 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatMintColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.58, 0.45, 0.37 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatNavyBlueColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.08, 0.85, 0.90 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatOrangeColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.90, 0.49, 0.96 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatPinkColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.83, 0.45, 0.37 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatPlumColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.62, 0.24, 0.95 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatPowderBlueColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.70, 0.52, 0.77 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatPurpleColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.02, 0.74, 0.91 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatRedColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.12, 0.25, 0.94 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatSandColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.57, 0.76, 0.86 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatSkyBlueColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.54, 0.55, 0.51 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatTealColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.99, 0.53, 0.94 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatWatermelonColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.53, 0.02, 0.95 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatWhiteColor;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.13, 0.99, 1.00 and whose alpha value is 1.0.
+ *
+ *  @return A flat UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatYellowColor;
+
+#pragma mark - Dark Shades
+
+/**
+ *  Returns a flat color object whose HSB values are 0.00, 0.00, 0.15 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatBlackColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.62, 0.56, 0.51 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatBlueColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.07, 0.45, 0.31 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatBrownColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.07, 0.34, 0.56 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatCoffeeColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.38, 0.44, 0.31 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatForestGreenColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.51, 0.10, 0.55 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatGrayColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.40, 0.78, 0.68 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatGreenColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.21, 0.81, 0.69 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatLimeColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.78, 0.61, 0.68 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatMagentaColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.01, 0.68, 0.40 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatMaroonColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.47, 0.86, 0.63 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatMintColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.58, 0.45, 0.31 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatNavyBlueColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.07, 1.00, 0.83 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatOrangeColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.91, 0.57, 0.83 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatPinkColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.83, 0.46, 0.31 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatPlumColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.62, 0.28, 0.84 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatPowderBlueColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.70, 0.56, 0.64 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatPurpleColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.02, 0.78, 0.75 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatRedColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.12, 0.30, 0.84 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatSandColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.57, 0.78, 0.73 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatSkyBlueColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.54, 0.54, 0.45 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatTealColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.99, 0.61, 0.85 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatWatermelonColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.57, 0.05, 0.78 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatWhiteColorDark;
+
+/**
+ *  Returns a flat color object whose HSB values are 0.11, 1.00, 1.00 and whose alpha value is 1.0.
+ *
+ *  @return A flat @c UIColor object in the HSB colorspace.
+ */
++ (UIColor *)flatYellowColorDark;
+
+
 @end
 NS_ASSUME_NONNULL_END
--- NXCategories/UIKit/UIColor+NXAdd.m
+++ NXCategories/UIKit/UIColor+NXAdd.m
@@ -9,28 +9,10 @@
 #import "UIColor+NXAdd.h"
 #import "NSString+NXAdd.h"
 
+
+#define hsb(h,s,b) [UIColor colorWithHue:h/360.0f saturation:s/100.0f brightness:b/100.0f alpha:1.0]
+
 @implementation UIColor (NXAdd)
-
-+ (UIColor *)colorWithRGB:(uint32_t)rgbValue {
-    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16) / 255.0f
-                           green:((rgbValue & 0xFF00) >> 8) / 255.0f
-                            blue:(rgbValue & 0xFF) / 255.0f
-                           alpha:1];
-}
-
-+ (UIColor *)colorWithRGBA:(uint32_t)rgbaValue {
-    return [UIColor colorWithRed:((rgbaValue & 0xFF000000) >> 24) / 255.0f
-                           green:((rgbaValue & 0xFF0000) >> 16) / 255.0f
-                            blue:((rgbaValue & 0xFF00) >> 8) / 255.0f
-                           alpha:(rgbaValue & 0xFF) / 255.0f];
-}
-
-+ (UIColor *)colorWithRGB:(uint32_t)rgbValue alpha:(CGFloat)alpha {
-    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16) / 255.0f
-                           green:((rgbValue & 0xFF00) >> 8) / 255.0f
-                            blue:(rgbValue & 0xFF) / 255.0f
-                           alpha:alpha];
-}
 
 static inline NSUInteger hexStrToInt(NSString *str) {
     uint32_t result = 0;
@@ -70,12 +52,229 @@ static BOOL hexStrToRGBA(NSString *str,
     return YES;
 }
 
++ (UIColor *)colorWithRGB:(uint32_t)rgbValue {
+    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16) / 255.0f
+                           green:((rgbValue & 0xFF00) >> 8) / 255.0f
+                            blue:(rgbValue & 0xFF) / 255.0f
+                           alpha:1];
+}
+
++ (UIColor *)colorWithRGBA:(uint32_t)rgbaValue {
+    return [UIColor colorWithRed:((rgbaValue & 0xFF000000) >> 24) / 255.0f
+                           green:((rgbaValue & 0xFF0000) >> 16) / 255.0f
+                            blue:((rgbaValue & 0xFF00) >> 8) / 255.0f
+                           alpha:(rgbaValue & 0xFF) / 255.0f];
+}
+
++ (UIColor *)colorWithRGB:(uint32_t)rgbValue alpha:(CGFloat)alpha {
+    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16) / 255.0f
+                           green:((rgbValue & 0xFF00) >> 8) / 255.0f
+                            blue:(rgbValue & 0xFF) / 255.0f
+                           alpha:alpha];
+}
+
 + (instancetype)colorWithHexString:(NSString *)hexStr {
     CGFloat r, g, b, a;
     if (hexStrToRGBA(hexStr, &r, &g, &b, &a)) {
         return [UIColor colorWithRed:r green:g blue:b alpha:a];
     }
     return nil;
+}
+
+#pragma mark - Chameleon - Light Shades
+
++ (UIColor *)flatBlackColor {
+    return hsb(0, 0, 17);
+}
+
++ (UIColor *)flatBlueColor {
+    return hsb(224, 50, 63);
+}
+
++ (UIColor *)flatBrownColor {
+    return hsb(24, 45, 37);
+}
+
++ (UIColor *)flatCoffeeColor {
+    return hsb(25, 31, 64);
+}
+
++ (UIColor *)flatForestGreenColor {
+    return hsb(138, 45, 37);
+}
+
++ (UIColor *)flatGrayColor {
+    return hsb(184, 10, 65);
+}
+
++ (UIColor *)flatGreenColor {
+    return hsb(145, 77, 80);
+}
+
++ (UIColor *)flatLimeColor {
+    return hsb(74, 70, 78);
+}
+
++ (UIColor *)flatMagentaColor {
+    return hsb(283, 51, 71);
+}
+
++ (UIColor *)flatMaroonColor {
+    return hsb(5, 65, 47);
+}
+
++ (UIColor *)flatMintColor {
+    return hsb(168, 86, 74);
+}
+
++ (UIColor *)flatNavyBlueColor {
+    return hsb(210, 45, 37);
+}
+
++ (UIColor *)flatOrangeColor {
+    return hsb(28, 85, 90);
+}
+
++ (UIColor *)flatPinkColor {
+    return hsb(324, 49, 96);
+}
+
++ (UIColor *)flatPlumColor {
+    return hsb(300, 45, 37);
+}
+
++ (UIColor *)flatPowderBlueColor {
+    return hsb(222, 24, 95);
+}
+
++ (UIColor *)flatPurpleColor {
+    return hsb(253, 52, 77);
+}
+
++ (UIColor *)flatRedColor {
+    return hsb(6, 74, 91);
+}
+
++ (UIColor *)flatSandColor {
+    return hsb(42, 25, 94);
+}
+
++ (UIColor *)flatSkyBlueColor {
+    return hsb(204, 76, 86);
+}
+
++ (UIColor *)flatTealColor {
+    return hsb(195, 55, 51);
+}
+
++ (UIColor *)flatWatermelonColor {
+    return hsb(356, 53, 94);
+}
+
++ (UIColor *)flatWhiteColor {
+    return hsb(192, 2, 95);
+}
+
++ (UIColor *)flatYellowColor {
+    return hsb(48, 99, 100);
+}
+
+#pragma mark - Chameleon - Dark Shades
+
++ (UIColor *)flatBlackColorDark {
+    return hsb(0, 0, 15);
+}
+
++ (UIColor *)flatBlueColorDark {
+    return hsb(224, 56, 51);
+}
+
++ (UIColor *)flatBrownColorDark {
+    return hsb(25, 45, 31);
+}
+
++ (UIColor *)flatCoffeeColorDark {
+    return hsb(25, 34, 56);
+}
+
++ (UIColor *)flatForestGreenColorDark {
+    return hsb(135, 44, 31);
+}
+
++ (UIColor *)flatGrayColorDark {
+    return hsb(184, 10, 55);
+}
+
++ (UIColor *)flatGreenColorDark {
+    return hsb(145, 78, 68);
+}
+
++ (UIColor *)flatLimeColorDark {
+    return hsb(74, 81, 69);
+}
+
++ (UIColor *)flatMagentaColorDark {
+    return hsb(282, 61, 68);
+}
+
++ (UIColor *)flatMaroonColorDark {
+    return hsb(4, 68, 40);
+}
+
++ (UIColor *)flatMintColorDark {
+    return hsb(168, 86, 63);
+}
+
++ (UIColor *)flatNavyBlueColorDark {
+    return hsb(210, 45, 31);
+}
+
++ (UIColor *)flatOrangeColorDark {
+    return hsb(24, 100, 83);
+}
+
++ (UIColor *)flatPinkColorDark {
+    return hsb(327, 57, 83);
+}
+
++ (UIColor *)flatPlumColorDark {
+    return hsb(300, 46, 31);
+}
+
++ (UIColor *)flatPowderBlueColorDark {
+    return hsb(222, 28, 84);
+}
+
++ (UIColor *)flatPurpleColorDark {
+    return hsb(253, 56, 64);
+}
+
++ (UIColor *)flatRedColorDark {
+    return hsb(6, 78, 75);
+}
+
++ (UIColor *)flatSandColorDark {
+    return hsb(42, 30, 84);
+}
+
++ (UIColor *)flatSkyBlueColorDark {
+    return hsb(204, 78, 73);
+}
+
++ (UIColor *)flatTealColorDark {
+    return hsb(196, 54, 45);
+}
+
++ (UIColor *)flatWatermelonColorDark {
+    return hsb(358, 61, 85);
+}
+
++ (UIColor *)flatWhiteColorDark {
+    return hsb(204, 5, 78);
+}
+
++ (UIColor *)flatYellowColorDark {
+    return hsb(40, 100, 100);
 }
 
 @end
