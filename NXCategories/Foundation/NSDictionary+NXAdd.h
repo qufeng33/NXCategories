2be1482d4ae6c32d75192c6f36fde0d365379b61
nightx <qufeng33@hotmail.com>
2016-05-31 23:37:18 +0800

    添加了/**

--- NXCategories/Foundation/NSDictionary+NXAdd.h
+++ NXCategories/Foundation/NSDictionary+NXAdd.h
@@ -17,4 +17,34 @@
  */
 - (NSDictionary *)removeNullValue;
 
+/**
+ *  判断字典对于给定Key是否有内容
+ *
+ *  @param key 给定的Key
+ *
+ *  @return YES 存在 NO 不存在
+ */
+- (BOOL)hasKey:(NSString *)key;
+
+/**
+ *  与另一个字典合并
+ *
+ *  @param dict 将要合并过来的字典
+ *
+ *  @return 合并后的字典
+ */
+- (NSDictionary *)dictionaryByMergingWith:(NSDictionary *)dict;
+
+/**
+ *  合并两个字典
+ *
+ *  @param dict1 第一个字典
+ *  @param dict2 第二个字典
+ *
+ *  @return 合并之后得到的字典
+ */
++ (NSDictionary *)dictionaryByMerging:(NSDictionary *)dict1 with:(NSDictionary *)dict2;
+
+
+
 @end
--- NXCategories/Foundation/NSDictionary+NXAdd.m
+++ NXCategories/Foundation/NSDictionary+NXAdd.m
@@ -31,4 +31,27 @@
     return [[NSDictionary alloc] initWithDictionary:mdic];
 }
 
+- (BOOL)hasKey:(NSString *)key {
+    return [self objectForKey:key] != nil;
+}
+
+- (NSDictionary *)dictionaryByMergingWith:(NSDictionary *)dict {
+    return [[self class] dictionaryByMerging:self with: dict];
+}
+
++ (NSDictionary *)dictionaryByMerging:(NSDictionary *)dict1 with:(NSDictionary *)dict2 {
+    NSMutableDictionary * result = [NSMutableDictionary dictionaryWithDictionary:dict1];
+    [dict2 enumerateKeysAndObjectsUsingBlock: ^(id key, id obj, BOOL *stop) {
+        if (![dict1 objectForKey:key]) {
+            if ([obj isKindOfClass:[NSDictionary class]]) {
+                NSDictionary * newVal = [[dict1 objectForKey: key] dictionaryByMergingWith: (NSDictionary *) obj];
+                [result setObject: newVal forKey: key];
+            } else {
+                [result setObject: obj forKey: key];
+            }
+        }
+    }];
+    return (NSDictionary *) [result mutableCopy];
+}
+
 @end
